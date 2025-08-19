fc47d0ef430d1c4fa0369507c64b922e3b62a615
Mazen PHP <mazenhomei123@gmail.com>
2025-08-19 11:25:48 +0300

    some updates

---
+++ app/Filament/Resources/Countries/CountryResource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources\Countries;
+
+use App\Filament\Resources\Countries\Pages\CreateCountry;
+use App\Filament\Resources\Countries\Pages\EditCountry;
+use App\Filament\Resources\Countries\Pages\ListCountries;
+use App\Filament\Resources\Countries\Schemas\CountryForm;
+use App\Filament\Resources\Countries\Tables\CountriesTable;
+use App\Models\Country;
+use BackedEnum;
+use UnitEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class CountryResource extends Resource
+{
+    protected static ?string $model = Country::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $navigationLabel = 'Country';
+
+    protected static ?string $modelLabel = 'Employees Country';
+
+    protected static UnitEnum|string|null $navigationGroup = 'System Management';
+
+    public static function form(Schema $schema): Schema
+    {
+        return CountryForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return CountriesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListCountries::route('/'),
+            'create' => CreateCountry::route('/create'),
+            'edit' => EditCountry::route('/{record}/edit'),
+        ];
+    }
+}
